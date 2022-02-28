e156ed9b2171fdeb3472a03310582630fcc221f3
Khurkhartsaga Munkhbold <60725072+Saga910@users.noreply.github.com>
2022-02-27 20:37:29 -0800

    Add files via upload

--- client.js
+++ client.js
@@ -1,23 +1,24 @@
-const xhttp = new XMLHttpRequest();
-const list = document.getElementById("list");
 
 function send(){
+    const xhttp = new XMLHttpRequest();
+    const list = document.getElementById("list");
     let name = document.getElementById("name").value;
     let score = document.getElementById("score").value;    
+
     let number = parseInt(score);
-    let req = "https://alexgiasson.me/comp4537/labs/5/api/definitions?name=" + name + "&score=" + number;
+    let req = "https://alexgiasson.me/comp4537/labs/5/?name=" + name + "&score=" + number;
+    
     xhttp.open("POST", req, true);
-    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhttp.send();
+    xhttp.setRequestHeader('Content-Type', 'text/html; charset=utf-8');
     xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let a = this.responseText;
-            let x = JSON.parse(a);
-            list.innerHTML = x.name + ": " + x.score;
+        if(this.readyState == 4){
+            console.log(this.responseText);
+
         } else if(this.readyState == 4 && this.status == 404){
             list.innerHTML = "Error";
         }
     }
+    xhttp.send(null); 
 }
 
 
--- read.js
+++ read.js
@@ -1,17 +1,18 @@
+let xhttp = new XMLHttpRequest();
 get();
-const xhttp = new XMLHttpRequest();
+
 const list = document.getElementById("list");
 
 function get(){
-    let req = "https://alexgiasson.me/comp4537/labs/5/api/definitions?name";
+    let req = "https://alexgiasson.me/comp4537/labs/5";
     xhttp.open("GET", req, true);
-    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.setRequestHeader("Content-Type", "text/html; charset=utf-8");
     xhttp.send();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let a = this.responseText;
             let x = JSON.parse(a);
-            list.innerHTML = x.name + ": "  + x.score;
+            console.log(x);
         } else if(this.readyState == 4 && this.status == 404){
             list.innerHTML = "Error";
         }
